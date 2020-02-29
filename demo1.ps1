e87d713d0ca18ece46faa0177f2940a8f9ec150e
David Gardiner <david@gardiner.net.au>
2020-02-29 17:49:38 +1030

    Demo script for PowerShell 7

---
+++ demo1.ps1
@@ -0,0 +1,115 @@
+#Requires -Version 7
+
+# SETUP
+$ErrorView = 'NormalView'
+Get-ExperimentalFeature | Disable-ExperimentalFeature
+
+
+
+
+
+
+
+# ForEach-Object -Parallel
+
+Measure-Command {
+
+    Get-ChildItem c:\dev\git |
+    ForEach-Object #-parallel
+    {
+
+        for ($i = 0; $i -lt 100000; $i++) {
+            $j += 1
+        }
+    }
+}
+
+##
+# -split 
+
+"a b c d" -split " "
+"a b c d" -split " ", 2
+"a b c d" -split " ", -2
+
+# Select-String emphasis
+Get-ChildItem C:\dev\git\au-packages -Recurse *.nuspec | Select-String title
+
+Get-ChildItem C:\dev\git\au-packages -Recurse *.nuspec | Select-String title -NoEmphasis
+
+# Select-String -Raw
+Get-ChildItem C:\dev\git\au-packages -Recurse *.nuspec | Select-String title -Raw
+
+# Select-String -Culture
+Get-ChildItem C:\dev\git\au-packages -Recurse *.nuspec | Select-String -SimpleMatch title -Culture zh-CN
+
+# Group Policy
+InstallPSCorePolicyDefinitions.ps1
+
+# ActionPreference.Break
+
+$ErrorActionPreference = "Break"
+Write-Host "1"
+Write-Verbose "Verbose"
+Write-Error "Oh dear"
+
+# Get-Error
+throw "oh dear"
+Get-Error
+
+# Pipeline chain
+(ping -x) && (ping localhost)
+(ping -x) || (ping localhost)
+
+# Null coalescing
+
+$a = "a value"
+$a ?? "fallback value"
+$a = $null
+$a ?? "fallback value"
+
+$a ??= "fallback value"
+
+# Null conditional
+$f = (Get-Item .\demo1.ps1)
+$f.Directory.Name
+$f = (Get-Item demo2.ps1 -ErrorAction Ignore)
+$f?.Directory?.Name
+
+# Experimental features
+Get-ExperimentalFeature
+Enable-ExperimentalFeature -Name PSNullConditionalOperators
+
+# Tab completion for enums and validateset
+[System.Drawing.KnownColor] $c
+$ErrorActionPreference
+
+# ConvertFrom-SecureString -AsPlainText
+$cred = Get-Credential
+$cred.Password
+
+ConvertFrom-SecureString -AsPlainText -SecureString $cred.Password
+
+# Ternary operator
+$answer = [Datetime]::Now.Minute -gt 30 ? "minutes to" : "minutes past"
+
+# Get-Hotfix (missing in Core 6)
+Get-HotFix
+
+# $ErrorView
+$i = 0
+$i.ToString2()
+
+# Update-List
+$a = [PSCustomObject]@{
+    Name = 'Test';
+    List = [System.Collections.ArrayList]::new( @("a", "b", "c"))
+}
+
+$a | Update-List -Property List -Add "d"
+
+
+# Out-Gridview (missing in Core 6)
+Get-ChildItem .. | Out-GridView
+
+# Clear-RecycleBin
+Clear-RecycleBin
